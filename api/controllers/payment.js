8682e6ab6527a752256b53a1e4bfc9455f20f6a6
Emerson <emerson.yur@gmail.com>
2020-08-24 13:20:39 +0200

    Fix Real Payments

--- api/controllers/payment.js
+++ api/controllers/payment.js
@@ -2,8 +2,8 @@ const mpesa = require('mpesa-node-api');
 module.exports = (app) => {
   const pay = (req, resp) => {
     mpesa.initializeApi({
-      baseUrl: process.env.MPESA_API_HOST,
       origin: process.env.MPESA_ORIGIN,
+      baseUrl: process.env.MPESA_API_HOST,
       apiKey: process.env.MPESA_API_KEY,
       publicKey: process.env.MPESA_PUBLIC_KEY,
       serviceProviderCode: process.env.MPESA_SERVICE_PROVIDER_CODE,
@@ -12,17 +12,15 @@ module.exports = (app) => {
     if (data) {
       mpesa
         .initiate_c2b(
-          /* amount */ data.amount,
-          /* msisdn */ Number(`258${data.phone}`),
-          /* transaction ref */ 'T12344CAA4345',
-          /*3rd party ref*/ 'ref22323'
+          data.amount,
+          Number(`258${data.phone}`),
+          `TEST${Math.round(Math.random() * 100000000)}`,
+          `Test${Math.round(Math.random() * 100000000)}`
         )
         .then(function (response) {
-          // logging the response
           resp.send(response);
         })
         .catch(function (error) {
-          // TODO: handle errors
           resp.send(error);
         });
     } else {
--- api/controllers/test.js
+++ api/controllers/test.js
@@ -2,28 +2,25 @@ const mpesa = require('mpesa-node-api');
 module.exports = (app) => {
   const pay = (req, resp) => {
     mpesa.initializeApi({
-      baseUrl: process.env.MPESA_API_HOST,
       origin: process.env.MPESA_ORIGIN,
+      baseUrl: process.env.MPESA_TEST_API_HOST,
       apiKey: process.env.MPESA_TEST_API_KEY,
       publicKey: process.env.MPESA_TEST_PUBLIC_KEY,
       serviceProviderCode: process.env.MPESA_TEST_SERVICE_PROVIDER_CODE,
     });
     const data = { ...req.body };
-    const transaction = { id: 'T123443CAA423', thirdPartyID: 'Teste1' };
     if (data) {
       mpesa
         .initiate_c2b(
           data.amount,
           Number(`258${data.phone}`),
-          transaction.id,
-          transaction.thirdPartyID
+          `TEST${Math.round(Math.random() * 100000000)}`,
+          `Test${Math.round(Math.random() * 100000000)}`
         )
         .then(function (response) {
-          // logging the response
           resp.send(response);
         })
         .catch(function (error) {
-          // TODO: handle errors
           resp.send(error);
         });
     } else {
